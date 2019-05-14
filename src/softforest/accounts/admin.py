965e8ab13beed9aceb8a392c8f1c374db58f5cc2
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-05-14 15:44:26 +0500

    structure changed

--- src/softforest/accounts/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from . import models
-
-# Register your models here.
-
-admin.site.register(models.User)
-admin.site.register(models.Recommendation)
-admin.site.register(models.Profile)
