41addd9a4bb6da77899230e9a9c8ed257a430ae1
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-05-16 20:20:22 +0500

    update server setup

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 # TODO: Set to URL of git repo.
-PROJECT_GIT_URL='https://github.com/Zayn484/softForest-rest-api.git'
+PROJECT_GIT_URL='https://github.com/Zayn484/softforest-rest-api.git'
 
 PROJECT_BASE_PATH='/usr/local/apps'
 VIRTUALENV_BASE_PATH='/usr/local/virtualenvs'
