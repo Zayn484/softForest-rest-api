4a23ecb60bbbdef3a87acaf0e67e6c80268c82bc
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-05-18 00:06:20 +0500

    ser

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -16,9 +16,9 @@ echo "Installing dependencies..."
 apt-get update
 apt-get install -y python3-dev python3-venv sqlite python-pip supervisor nginx git
 apt-get install redis
-apt install daphne
-apt install pathlib
-apt install channels
+pip install daphne
+pip install pathlib
+pip install -U channels
 
 mkdir -p $PROJECT_BASE_PATH
 git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/softforest-rest-api
