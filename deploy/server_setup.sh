ebd592dcdd2c2d6d67c0dd079d65a3e98096cbf7
Zain Ul Hassan <zain.hassan1212@gmail.com>
2019-05-18 12:02:45 +0500

    updates

--- deploy/server_setup.sh
+++ deploy/server_setup.sh
@@ -26,12 +26,13 @@ git clone $PROJECT_GIT_URL $PROJECT_BASE_PATH/softforest-rest-api
 
 mkdir -p $VIRTUALENV_BASE_PATH
 python3 -m venv $VIRTUALENV_BASE_PATH/softforest
+source /usr/local/virtualenvs/softforest/bin/activate
 
 $VIRTUALENV_BASE_PATH/softforest/bin/pip install -r $PROJECT_BASE_PATH/softforest-rest-api/requirements.txt
 
 # Run migrations
 cd $PROJECT_BASE_PATH/softforest-rest-api/src
-python manage.py migrate
+python3 manage.py migrate
 
 # Setup Supervisor to run our uwsgi process.
 cp $PROJECT_BASE_PATH/softforest-rest-api/deploy/supervisor_softforest_api.conf /etc/supervisor/conf.d/softforest.conf
